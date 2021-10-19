3afe4843826d78bed71d86bd8ee3f651dc117485
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-19 11:13:20 +0100

    Acknowledge David

---
+++ integrate_graph.py
@@ -0,0 +1,12 @@
+import statistics
+
+mm_to_val = 1.0/17.0
+values = [7, 17.5, 22, 22.5, 21.5, 17.5, 14, 11, 8, 6]
+print(statistics.mean(values)*mm_to_val)
+
+mm_to_val = 1.0/26.0
+values = [10, 20, 29, 38, 40, 53, 59, 64, 67,70,71,69,67,63,61,57,52,47,43,39,37,32,29,25,22]
+print(statistics.mean(values)*mm_to_val)
+
+
+
